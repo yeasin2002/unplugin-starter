afd97ca79123d8c8334d870a49281f421ce81116
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-15 16:08:51 +0600

    feat: added unplugin APIs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export * from './utils'
+import { createUnplugin, type UnpluginInstance } from 'unplugin'
+import { resolveOptions, type Options } from './core/options'
+
+export const Starter: UnpluginInstance<Options | undefined, false> = createUnplugin(
+  (rawOptions = {}) => {
+    const options = resolveOptions(rawOptions)
+
+    const name = 'unplugin-starter'
+    return {
+      name,
+      enforce: options.enforce,
+
+      transform: {
+        filter: {
+          id: { include: options.include, exclude: options.exclude },
+        },
+        handler(code) {
+          return `// unplugin-starter injected\n${code}`
+        },
+      },
+    }
+  }
+)
